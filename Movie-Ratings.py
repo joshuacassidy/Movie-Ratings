861f25a415db3a3a59ace7c12c6f83b30f7543ee
joshuacassidy <Joshcassidy79@gmail.com>
2017-07-08 20:53:56 +0100

    Optimized and Styled MovieBudgetDistribution

--- Movie-Ratings.py
+++ Movie-Ratings.py
@@ -4,13 +4,10 @@ import seaborn as sns
 
 movies = pd.read_csv('Movie-Ratings-Data.csv')
 movies.columns = ['Film', 'Genre', 'CriticRating', 'AudienceRating','BudgetMillions','Year']
-
-movies.Film = movies.Film.astype('category')
 movies.Genre = movies.Genre.astype('category')
-movies.Year = movies.Year.astype('category')
-
 distribution = [movies.AudienceRating,movies.CriticRating]
-sns.set_style("white")
+listOfMovies = list()
+mylabels = list()
 
 def showGraph():    
     plt.show()
@@ -23,37 +20,39 @@ def Hist(i):
     plt.gcf().canvas.set_window_title('Distribution of Audience and Critic Ratings')
 
 def AudienceAndCriticRating():
+    sns.set_style("white")
     [Hist(i)  for i in distribution]
     showGraph()
 
-sns.set_style("dark",{"axes.facecolor":"black"})
-f, axes = plt.subplots(2,2, figsize=(12,8))
-
-sns.set_style("dark",{"axes.facecolor":"black"})
-
 
 def FacetGrid():
-    [Kde(i)  for i in range(0,2)]
+    sns.set_style("dark",{"axes.facecolor":"black"})
+    f, axes = plt.subplots(2,2, figsize=(12,8))
+    [Kde(i,axes)  for i in range(0,2)]
     sns.violinplot(data=movies, x = 'Year', y='BudgetMillions', ax=axes[1,0],palette="YlOrRd")
     sns.kdeplot(movies.CriticRating,movies.AudienceRating,shade=True,shade_lowest=False,cmap='Blues_r',ax=axes[1,1])
     sns.kdeplot(movies.CriticRating,movies.AudienceRating,cmap='gist_gray_r', ax=axes[1,1])
+    plt.gcf().canvas.set_window_title('Facet Grid')
     showGraph()
 
-def Kde(i):
+def Kde(i,axes):
     sns.kdeplot(movies.BudgetMillions , distribution[i], ax=axes[0,i],shade=True, shade_lowest=True,cmap='inferno')
     sns.kdeplot(movies.BudgetMillions , distribution[i],ax=axes[0,i],cmap='cool')
 
 
+def budget(gen):
+    listOfMovies.append(movies[movies.Genre == gen].BudgetMillions)
+    mylabels.append(gen)
 
-list1 = list()
-mylabels = list()
-for gen in movies.Genre.cat.categories:
-    list1.append(movies[movies.Genre == gen].BudgetMillions)
-    
-fig,ax = plt.subplots()
-h = plt.hist(list1,bins=30, stacked=True,rwidth=1, label=mylabels)
-plt.legend(prop={'size':12})
-plt.show()
+def MovieBudgetDistribution():
+    sns.set_style("whitegrid",{"axes.facecolor":"white"})
+    [budget(gen) for gen in movies.Genre.cat.categories]
+    fig,ax = plt.subplots()
+    plt.hist(listOfMovies,bins=30, stacked=True,rwidth=1, label=mylabels)
+    plt.title("Movie Budget Distribution", fontsize=25,color="Gray",fontname="Impact")
+    plt.legend(prop={'size':12})
+    plt.show()
 
 AudienceAndCriticRating()
 FacetGrid()
+MovieBudgetDistribution()
